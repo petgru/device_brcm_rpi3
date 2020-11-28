dd63965277edcf7f0f180d171a47f20cab745518
Peter Grundström (jpc632) <peter.grundstrom@gmail.com>
2020-11-28 17:56:51 +0100

    First test with automotive product

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,7 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/rpi3.mk
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/rpi3.mk \
+	$(LOCAL_DIR)/rpi3-automotive.mk
 
-COMMON_LUNCH_CHOICES := rpi3-eng
+COMMON_LUNCH_CHOICES := \
+	rpi3-eng \
+	rpi3-automotive
